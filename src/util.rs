196273496be7a2560246dce88fe2d9c9487ab91d
Yuki Kishimoto <yukikishimoto@proton.me>
2022-06-27 15:46:22 +0200

    Reduce random_id len

--- src/util.rs
+++ src/util.rs
@@ -6,7 +6,7 @@ use rand::{distributions::Alphanumeric, Rng};
 pub fn random_id() -> String {
     rand::thread_rng()
         .sample_iter(&Alphanumeric)
-        .take(32)
+        .take(20)
         .map(char::from)
         .collect()
 }
