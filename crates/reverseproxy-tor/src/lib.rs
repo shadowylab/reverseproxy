7f5e50ddd7fae948aab4bdba1bc761abdf4731b3
Yuki Kishimoto <yukikishimoto@proton.me>
2022-07-18 11:56:26 +0200

    Update reverseproxy-tor module

--- crates/reverseproxy-tor/src/lib.rs
+++ crates/reverseproxy-tor/src/lib.rs
@@ -1,16 +1,33 @@
 // Copyright (c) 2022 Yuki Kishimoto
 // Distributed under the MIT software license
 
-use libtor::{Tor, TorFlag};
+use libtor::{Tor as LibTor, TorFlag};
 
-pub fn run_tor() {
-    match Tor::new()
-        .flag(TorFlag::DataDirectory("/tmp/reverseproxy".into()))
-        .flag(TorFlag::SocksPort(19054))
-        .flag(TorFlag::Hush())
-        .start()
-    {
-        Ok(r) => log::info!("tor exit result: {}", r),
-        Err(e) => log::error!("tor error: {}", e),
-    };
+pub struct Tor {
+    data_dir: String,
+    socks_port: u16,
+}
+
+impl Tor {
+    pub fn new(data_dir: String, socks_port: u16) -> Self {
+        Self {
+            data_dir,
+            socks_port,
+        }
+    }
+
+    pub fn start(&self) {
+        match LibTor::new()
+            .flag(TorFlag::DataDirectory(self.data_dir.clone()))
+            .flag(TorFlag::SocksPort(self.socks_port))
+            //.flag(TorFlag::Hush())
+            .start()
+        {
+            Ok(_) => {
+                log::info!("Tor exited");
+                std::process::exit(0x1);
+            }
+            Err(e) => log::error!("Tor error: {}", e),
+        };
+    }
 }
