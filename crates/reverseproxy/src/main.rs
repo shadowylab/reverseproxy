03afb8ad2b2c5a6546bf95a4423805d962fe868f
Yuki Kishimoto <yukikishimoto@proton.me>
2022-07-18 11:57:01 +0200

    Remove tor feature

--- crates/reverseproxy/src/main.rs
+++ crates/reverseproxy/src/main.rs
@@ -2,27 +2,24 @@
 // Distributed under the MIT software license
 
 use std::net::SocketAddr;
-#[cfg(feature = "tor")]
 use std::net::{Ipv4Addr, SocketAddrV4};
 
 use anyhow::Result;
 use reverseproxy_config::{Args, Parser};
 use reverseproxy_logger::Logger;
 use reverseproxy_tcp::TcpReverseProxy;
-#[cfg(feature = "tor")]
-use reverseproxy_tor::run_tor;
+use reverseproxy_tor::Tor;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args: Args = Args::parse();
     Logger::init();
 
-    #[cfg(feature = "tor")]
     if args.use_tor {
-        tokio::task::spawn_blocking(run_tor);
+        let tor = Tor::new("/tmp/reverseproxy".into(), 19054);
+        tokio::task::spawn_blocking(move || tor.start());
     }
 
-    #[cfg(feature = "tor")]
     let socks5_proxy: Option<SocketAddr> = if args.use_tor {
         Some(SocketAddr::V4(SocketAddrV4::new(
             Ipv4Addr::LOCALHOST,
@@ -31,9 +28,6 @@ async fn main() -> Result<()> {
     } else {
         args.socks5_proxy
     };
-
-    #[cfg(not(feature = "tor"))]
-    let socks5_proxy: Option<SocketAddr> = args.socks5_proxy;
 
     TcpReverseProxy::new(args.local_addr, args.forward_addr, socks5_proxy)
         .run()
