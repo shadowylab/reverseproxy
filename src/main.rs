9af5afc7ff9c6e794e1d410b82d7918116b2a2da
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-07-02 09:52:49 -0400

    Enable support to `onion` addresses

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@
 mod config;
 mod logger;
 mod tcp;
+#[cfg(feature = "tor")]
+mod tor;
 
 use self::config::{Args, Parser};
 use self::logger::Logger;
 use self::tcp::TcpReverseProxy;
-#[cfg(feature = "tor")]
-use arti_client::{TorClient, TorClientConfig};
 
 type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
@@ -24,11 +24,8 @@ async fn main() -> Result<()> {
 
     #[cfg(feature = "tor")]
     if args.tor {
-        tracing::info!("Bootstraping embedded Tor client...");
-        let config = TorClientConfig::default();
-        let tor_client = TorClient::create_bootstrapped(config).await?;
-        reverse_proxy = reverse_proxy.tor(tor_client);
-        tracing::info!("Bootstrap completed");
+        let client = tor::init_client().await?;
+        reverse_proxy = reverse_proxy.tor(client);
     }
 
     reverse_proxy.socks5_proxy(args.socks5_proxy).run().await
---
+++ src/tor.rs
@@ -0,0 +1,26 @@
+// Copyright (c) 2022-2024 Yuki Kishimoto
+// Distributed under the MIT software license
+
+use arti_client::config::TorClientConfigBuilder;
+use arti_client::{TorClient, TorClientConfig};
+use tor_rtcompat::PreferredRuntime;
+
+use super::Result;
+
+pub async fn init_client() -> Result<TorClient<PreferredRuntime>> {
+    // Compose config
+    let mut config = TorClientConfigBuilder::default();
+    config.address_filter().allow_onion_addrs(true);
+    let config: TorClientConfig = config.build()?;
+
+    tracing::info!("Bootstraping embedded Tor client...");
+
+    let client = TorClient::builder()
+        .config(config)
+        .create_bootstrapped()
+        .await?;
+
+    tracing::info!("Bootstrap completed");
+
+    Ok(client)
+}
